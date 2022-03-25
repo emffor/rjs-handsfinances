4f42bcc32c8d10718e6c5d9ca0cf645420ab3b8a
emffor <98027423+emffor@users.noreply.github.com>
2022-03-25 16:34:29 -0300

    fix: installing styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  font-size: 64px;
+`;
+
 export function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+        <Title>Hello World</Title>
     </div>
   );
 }
