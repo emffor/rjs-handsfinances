9ef4627a7bed1c81d8900198ca7fe31587a0a99d
emffor <98027423+emffor@users.noreply.github.com>
2022-03-26 13:33:57 -0300

    fix: creating cype buttons

--- src/components/NewTransationModal/styles.ts
+++ src/components/NewTransationModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished'; //escurece uma cor
 
 //1rem = 16px
 
@@ -53,4 +54,51 @@ export const Container = styled.form`
             }
         }
     
+`;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid; //colocar do lado do outro.
+    grid-template-columns: 1fr 1fr;
+    gap: 0.25rem;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        //efeitos
+        
+        transition: filter 0.2s;
+            
+        /* 
+            assim css normal.
+            &:hover {
+            border-color: #aaa; //efeito de borda escurecida.
+        } */
+
+        //polished é um pacote de efeitos de css com styled components
+        //escurecer em 10% a cor #d7d7d7.
+        
+            &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')}; 
+            }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block; //assim consigo dar um margin nele.
+            margin-left: 1rem;
+            font-size: 1rem;
+        }
+    }
 `;
