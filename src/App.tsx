feea9bf5023023e3232683be4f17f0a6a759fad7
emffor <98027423+emffor@users.noreply.github.com>
2022-03-26 12:12:23 -0300

    fix: configuring creation modal
    Configurando modal e em header passando a função que está dentro de header componente filho alterando o estado através do do componente de funções.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-
 import { GlobalStyled } from "./styles/global";
 
+Modal.setAppElement('#root'); //só para acessibilidade.
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false);
+  }
+  
   return (
     <>
-        <Header />
+        {/* 
+            em header passando a função que está dentro de header 
+            componente filho alterando o estado através do do componente de funções.
+        */}
+        <Header 
+          onOpenNewTransactionModal={handleOpenNewTransactionModal}
+        />
         <Dashboard />
+        <Modal 
+            isOpen={isNewTransactionModalOpen} 
+            onRequestClose={handleCloseNewTransactionModal}
+        >
+            <h2>Cadastrar transação</h2>
+        </Modal>
+        
+
         <GlobalStyled />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,25 @@ import {
     Content, 
 } from './styles';
 
-export function Header(){
+//receber uma propriedade para receber a function que está dentro do componente App();
+
+interface HeaderProps {
+   onOpenNewTransactionModal: () => void; 
+   //essa função não vai receber nenhum parâmetro, por isso o tipo é vazio.  
+}
+
+export function Header({ onOpenNewTransactionModal } : HeaderProps){
+
+
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money"/>
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+                
             </Content>
         </Container>
     )
