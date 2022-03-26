0b753600759c5a11b8cdf9715b91014ede0f1bf8
emffor <98027423+emffor@users.noreply.github.com>
2022-03-26 10:39:18 -0300

    fix: finalizing the transaction table
    Usando <table> <thead> <tbody><tr> <td> <tbody>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyled } from "./styles/global";
 
 export function App() {
   return (
     <>
         <Header />
+        <Dashboard />
         <GlobalStyled />
     </>
   );
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    display: grid;
+    //3 colunas de tamanhos iguais
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem; //espaçamento entre os cards
+    margin-top: -10rem;
+
+    div {
+        background: var(--shape);
+        padding: 1.5rem 2rem;
+        border-radius: 0.25rem;
+        color: var(--text-title);
+    
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    strong {
+        display: block; //mias espaçamento
+        margin-top: 1rem;
+        font-size: 2rem;
+        font-weight: 500;
+        line-height: 3rem;
+    }
+
+    &.highlight-background {
+        background: var(--green);
+        color: var(--shape);
+    }
+  }  
+`;
